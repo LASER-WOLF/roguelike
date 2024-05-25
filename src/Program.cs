0a4bdf046e888e475d7372821e0219157ea5787a
Casper <casper.eide@gmail.com>
2024-05-25 22:31:35 +0200

    raylib/imgui setup

--- src/Program.cs
+++ src/Program.cs
@@ -5,7 +5,6 @@ internal class Program
     static void Main(string[] args)
     {
         //Game game = new Game();
-        //Test.Game game = new Test.Game();
-        Test.ImGuiTest imGui = new Test.ImGuiTest();
+        Test.Game game = new Test.Game();
     }
 }
--- tests/Game.cs
+++ tests/Game.cs
@@ -1,4 +1,6 @@
 using Raylib_cs;
+using rlImGui_cs;
+using ImGuiNET;
 
 namespace Test;
 
@@ -6,18 +8,40 @@ class Game
 {
     public Game()
     {
-        Raylib.InitWindow(800, 480, "Hello World");
+        // Raylib/ImGui init
+        //Raylib.SetConfigFlags(ConfigFlags.Msaa4xHint | ConfigFlags.VSyncHint | ConfigFlags.ResizableWindow);
+        Raylib.InitWindow(1280, 720, "Roguelike");
+        //Raylib.SetTargetFPS(30);
+        rlImGui.Setup(true);
 
+        // Raylib loop
         while (!Raylib.WindowShouldClose())
         {
+            // Raylib start
             Raylib.BeginDrawing();
             Raylib.ClearBackground(Color.White);
-
             Raylib.DrawText("Hello, world!", 12, 12, 20, Color.Black);
+            
+            // ImGui start
+            rlImGui.Begin();
 
+            // ImGui content
+            ImGui.ShowDemoWindow();
+            // if (ImGui.Begin("Simple Window"))
+            // {
+            //     ImGui.TextUnformatted("Icon text " + IconFonts.FontAwesome6.Book);
+            // }
+
+            // ImGui end
+            ImGui.End();
+            rlImGui.End();
+
+            // Raylib end
             Raylib.EndDrawing();
         }
 
+        // Raylib/Imgui exit
+        rlImGui.Shutdown();
         Raylib.CloseWindow();
     }
 }
